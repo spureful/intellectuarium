2104bd9fb8d24d14eef6d5415486038dd5d55a3c
spureful <rw1zz@yandex..ru>
2022-07-30 13:14:19 +0300

    ADD_ROUTES >> + add router >> + add menu >> + add empty number page

---
+++ src/components/Menu/MenuItem.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+.menu-card(
+    :class="{disabled: isMenuItemdisabled}"
+    @click.prevent="openMenu"
+    ) {{ content }}
+
+</template>
+
+<script>
+export default {
+    name: "MenuItem",
+    props: {
+        id: String,
+        path: String,
+        content: String,
+    },
+    computed: {
+        isMenuItemdisabled() {
+            return window.location.pathname === this.path;
+        }
+    },
+    methods: {
+        openMenu() {
+            this.$router.push(this.path);
+            location.reload();
+        }
+    }
+}
+</script>
+
+<style scoped> 
+    .menu-card {
+        padding: 15px;
+        border-radius: 15px;
+        font-size: 18px;
+        color: rgb(77, 73, 73);
+        background-color: blanchedalmond;
+        margin-right: 15px;
+        cursor: pointer;
+        transition: all .8s;
+    }
+
+    .menu-card:hover {
+        transform: scale(0.8);
+    }
+
+    .menu-card.disabled {
+        pointer-events: none;
+        opacity: 0.8;
+    }
+</style>
+
+
---
+++ src/components/Numbers/Numbers.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.numbers Number page 
+
+</template>
+
+<script>
+export default {
+  name: "Numbers", 
+}
+</script>
+
+<style scoped>
+</style>
